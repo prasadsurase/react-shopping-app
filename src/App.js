04352a06b51dd1097503fdb2c7d561393d2826a6
Prasad Surase <prasad.surase@coupa.com>
2021-05-07 09:34:37 +0530

    Added show page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop.jsx'
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/shop" component={ShopPage}/>
+        </Switch>
       </div>
     );
   }
